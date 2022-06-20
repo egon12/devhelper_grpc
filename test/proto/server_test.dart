8d9cd9d42b6d64b3ab7f4c6f991b293a95069622
Egon Firman <egon.firman@tokopedia.com>
2022-06-20 19:26:08 +0700

    refactor server test

--- test/proto/server_test.dart
+++ test/proto/server_test.dart
@@ -4,47 +4,41 @@ import 'package:devhelper_grpc/proto/hello.pb.dart' as pb;
 import 'package:grpc/grpc.dart';
 
 void main() {
-	test('hallo', () async {
-		await listen();
-		expect('hallo', 'hallo');
-		final msg = await conn();
-		expect(msg, 'hello Egon');
-	});
+  test('hallo', () async {
+    await listen();
+    expect('hallo', 'hallo');
+    final msg = await conn();
+    expect(msg, 'hello Egon');
+  });
 }
 
 class HelloImpl extends HelloServiceBase {
-	@override
+  @override
   Future<pb.Response> hello(ServiceCall call, Request req) async {
-		return pb.Response(
-				message: "hello " + req.name,
-		);
-	}
+    return pb.Response(
+      message: "hello " + req.name,
+    );
+  }
 }
 
 Future<void> listen() async {
+  final server = Server([HelloImpl()], const <Interceptor>[], CodecRegistry());
 
-	final server = Server(
-			[HelloImpl()], 
-			const<Interceptor>[], 
-			CodecRegistry()
-			);
-
-	await server.serve(port:50051);
-	print('Server listeneing on port ${server.port} ...');
+  await server.serve(port: 50053);
 }
 
 Future<String> conn() async {
-	final c = ClientChannel(
-			'localhost', 
-			port: 50051,
-			options: ChannelOptions(
-					credentials: ChannelCredentials.insecure(),
-					codecRegistry: CodecRegistry(),
-			),
-	);
+  final c = ClientChannel(
+    'localhost',
+    port: 50053,
+    options: ChannelOptions(
+      credentials: ChannelCredentials.insecure(),
+      codecRegistry: CodecRegistry(),
+    ),
+  );
 
-	final hc = HelloClient(c);
+  final hc = HelloClient(c);
 
-	final res = await hc.hello(Request(name: 'Egon'));
-	return res.message;
+  final res = await hc.hello(Request(name: 'Egon'));
+  return res.message;
 }
